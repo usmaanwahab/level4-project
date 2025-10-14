2a9b1a550017ba23451f85118e0b44095307d001
Usmaan Wahab <usmaanwahab11@outlook.com>
2025-10-14 16:55:44 +0100

    NOELLE exampel

--- llvm14/src/main.cpp
+++ llvm14/src/main.cpp
@@ -1,53 +1,69 @@
 #include "llvm/IR/Function.h"
-#include "llvm/IR/Module.h"
-#include "llvm/Passes/PassBuilder.h"
-#include "llvm/Passes/PassPlugin.h"
+#include "llvm/IR/LegacyPassManager.h"
+#include "llvm/Pass.h"
 #include "llvm/Support/raw_ostream.h"
-#include <llvm/IR/BasicBlock.h>
-#include <llvm/IR/Instructions.h>
+#include "llvm/Transforms/IPO/PassManagerBuilder.h"
+#include <algorithm>
 
-using namespace llvm;
+#include "arcana/noelle/core/NoellePass.hpp"
 
-struct ByeModule : PassInfoMixin<ByeModule> {
-  PreservedAnalyses run(Module &M, ModuleAnalysisManager &MAM) {
-    int store_count = 0;
-    int load_count = 0;
+using namespace arcana::noelle;
 
-    for (Function &F : M) {
-      errs() << "Hello: " << F.getName() << "\n";
-      for (BasicBlock &BB : F) {
-        for (Instruction &I : BB) {
-          if (isa<LoadInst>(I)) {
-            load_count++;
-          }
-          if (isa<StoreInst>(I)) {
-            store_count++;
-          }
-        }
-      }
+namespace {
+
+struct CAT : public ModulePass {
+  static char ID;
+
+  CAT() : ModulePass(ID) {}
+
+  bool doInitialization(Module &M) override { return false; }
+
+  bool runOnModule(Module &M) override {
+
+    /*
+     * Fetch NOELLE
+     */
+    auto &noelle = getAnalysis<NoellePass>().getNoelle();
+
+    /*
+     * Fetch set of alias analysis engines.
+     */
+    auto aaEngines = noelle.getAliasAnalysisEngines();
+
+    /*
+     * Print the engines.
+     */
+    errs() << "NOELLE's alias analysis engines used:\n";
+    for (auto e : aaEngines) {
+      errs() << "   " << e->getName() << "  " << e->getRawPointer() << "\n";
     }
 
-    errs() << "stores: " << store_count << "\nloades: " << load_count << "\n";
-    return PreservedAnalyses::all();
+    return false;
+  }
+
+  void getAnalysisUsage(AnalysisUsage &AU) const override {
+    AU.addRequired<NoellePass>();
   }
 };
+} // namespace
 
-llvm::PassPluginLibraryInfo getByePluginInfo() {
-  return {LLVM_PLUGIN_API_VERSION, "HelloWorld", LLVM_VERSION_STRING,
-          [](PassBuilder &PB) {
-            PB.registerPipelineParsingCallback(
-                [](StringRef Name, ModulePassManager &MPM,
-                   ArrayRef<PassBuilder::PipelineElement>) {
-                  if (Name == "hello-world") {
-                    MPM.addPass(ByeModule());
-                    return true;
-                  }
-                  return false;
-                });
-          }};
-}
+// Next there is code to register your pass to "opt"
+char CAT::ID = 0;
+static RegisterPass<CAT> X("CAT", "Simple user of the Noelle framework");
 
-extern "C" LLVM_ATTRIBUTE_WEAK ::llvm::PassPluginLibraryInfo
-llvmGetPassPluginInfo() {
-  return getByePluginInfo();
-}
+// Next there is code to register your pass to "clang"
+static CAT *_PassMaker = NULL;
+static RegisterStandardPasses _RegPass1(PassManagerBuilder::EP_OptimizerLast,
+                                        [](const PassManagerBuilder &,
+                                           legacy::PassManagerBase &PM) {
+                                          if (!_PassMaker) {
+                                            PM.add(_PassMaker = new CAT());
+                                          }
+                                        }); // ** for -Ox
+static RegisterStandardPasses
+    _RegPass2(PassManagerBuilder::EP_EnabledOnOptLevel0,
+              [](const PassManagerBuilder &, legacy::PassManagerBase &PM) {
+                if (!_PassMaker) {
+                  PM.add(_PassMaker = new CAT());
+                }
+              }); // ** for -O0
