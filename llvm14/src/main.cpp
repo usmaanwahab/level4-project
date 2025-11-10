4840a93e1ef7380aa2a692196c39e62dda7138b9
Usmaan Wahab <usmaanwahab11@outlook.com>
2025-11-10 15:57:33 +0000

    not working - trying to use SVF, dependency issues

--- llvm14/src/main.cpp
+++ llvm14/src/main.cpp
@@ -1,123 +1,159 @@
-#include "llvm/Pass.h"
 #include "llvm/IR/Function.h"
-#include "llvm/Support/raw_ostream.h"
 #include "llvm/IR/LegacyPassManager.h"
+#include "llvm/Pass.h"
+#include "llvm/Support/raw_ostream.h"
 #include "llvm/Transforms/IPO/PassManagerBuilder.h"
 #include <algorithm>
 
 #include "arcana/noelle/core/NoellePass.hpp"
+#include "svf/MemoryModel/PointerAnalysis.h"
 
 using namespace arcana::noelle;
 
-namespace
-{
+namespace {
 
-  class MyDependenceAnalysis : public DependenceAnalysis
-  {
-  public:
-    MyDependenceAnalysis()
-        : DependenceAnalysis("Example of data dependence analysis"),
-          c{0} {}
+class MyDependenceAnalysis : public DependenceAnalysis {
+public:
+  MyDependenceAnalysis()
+      : DependenceAnalysis("Example of data dependence analysis"), c{0} {}
 
-    bool canThereBeAMemoryDataDependence(Instruction *fromInst,
-                                         Instruction *toInst,
-                                         Function &f) override
-    {
-      errs() << this->prefix << "canThereBeAMemoryDataDependence: Function "
-             << f.getName() << "\n";
-      errs() << this->prefix << "canThereBeAMemoryDataDependence:   From "
-             << *fromInst << "\n";
-      errs() << this->prefix << "canThereBeAMemoryDataDependence:   To "
-             << *toInst << "\n";
-      errs() << this->prefix << "canThereBeAMemoryDataDependence:\n";
+  bool canThereBeAMemoryDataDependence(Instruction *fromInst,
+                                       Instruction *toInst,
+                                       Function &f) override {
+    errs() << this->prefix << "canThereBeAMemoryDataDependence: Function "
+           << f.getName() << "\n";
+    errs() << this->prefix << "canThereBeAMemoryDataDependence:   From "
+           << *fromInst << "\n";
+    errs() << this->prefix << "canThereBeAMemoryDataDependence:   To "
+           << *toInst << "\n";
+    errs() << this->prefix << "canThereBeAMemoryDataDependence:\n";
 
-      c++;
+    c++;
 
-      return true;
-    }
+    return true;
+  }
 
-    bool canThereBeAMemoryDataDependence(Instruction *fromInst,
-                                         Instruction *toInst,
-                                         LoopStructure &loop) override
-    {
-      auto entryInst = loop.getEntryInstruction();
-      auto f = fromInst->getFunction();
-      errs() << this->prefix
-             << "canThereBeAMemoryDataDependence: Loop at nesting level "
-             << loop.getNestingLevel() << ": " << *entryInst << "\n";
-      errs() << this->prefix << "canThereBeAMemoryDataDependence:   In function "
-             << f->getName() << "\n";
-      errs()
-          << this->prefix << "canThereBeAMemoryDataDependence:   Dependence from "
-          << *fromInst << "\n";
-      errs() << this->prefix << "canThereBeAMemoryDataDependence:   to "
-             << *toInst << "\n";
-      errs() << this->prefix << "canThereBeAMemoryDataDependence:\n";
+  bool canThereBeAMemoryDataDependence(Instruction *fromInst,
+                                       Instruction *toInst,
+                                       LoopStructure &loop) override {
+    auto entryInst = loop.getEntryInstruction();
+    auto f = fromInst->getFunction();
+    errs() << this->prefix
+           << "canThereBeAMemoryDataDependence: Loop at nesting level "
+           << loop.getNestingLevel() << ": " << *entryInst << "\n";
+    errs() << this->prefix << "canThereBeAMemoryDataDependence:   In function "
+           << f->getName() << "\n";
+    errs() << this->prefix
+           << "canThereBeAMemoryDataDependence:   Dependence from " << *fromInst
+           << "\n";
+    errs() << this->prefix << "canThereBeAMemoryDataDependence:   to "
+           << *toInst << "\n";
+    errs() << this->prefix << "canThereBeAMemoryDataDependence:\n";
 
-      c++;
+    c++;
 
-      return true;
-    }
+    return true;
+  }
 
-    ~MyDependenceAnalysis()
-    {
-      errs() << "The API has been invoked " << this->c << " times\n";
-    }
+  ~MyDependenceAnalysis() {
+    errs() << "The API has been invoked " << this->c << " times\n";
+  }
 
-  private:
-    std::string prefix;
-    uint64_t c;
-  };
+private:
+  std::string prefix;
+  uint64_t c;
+};
 
-  struct CAT : public ModulePass
-  {
-    static char ID;
+struct CAT : public ModulePass {
+  static char ID;
 
-    CAT() : ModulePass(ID) {}
+  CAT() : ModulePass(ID) {}
 
-    bool doInitialization(Module &M) override
-    {
-      return false;
-    }
+  bool doInitialization(Module &M) override { return false; }
 
-    bool runOnModule(Module &M) override
-    {
-      errs() << "Example: Start\n";
+  bool runOnModule(Module &M) override {
+    /*
+    Identify the load instructions that may execute after a given load
+    instruction, for all load instructions
+    */
+    auto &noelle = getAnalysis<NoellePass>().getNoelle();
+    auto dfe = noelle.getDataFlowEngine();
+    auto aliasAnalysisEngines = noelle.getAliasAnalysisEngines();
 
-      errs() << "Example:   Fetch NOELLE\n";
-      auto &noelle = getAnalysis<NoellePass>().getNoelle();
-
-      errs() << "Example:   Register my own data dependence analysis\n";
-      MyDependenceAnalysis myDepAnalysis{};
-      noelle.addAnalysis(&myDepAnalysis);
-
-      errs() << "Example:   Fetch the PDG\n";
-      auto PDG = noelle.getProgramDependenceGraph();
-
-      errs() << "Example:   Fetch the FDG of \"main\"\n";
-      auto fm = noelle.getFunctionsManager();
-      auto mainF = fm->getEntryFunction();
-      auto FDG = PDG->createFunctionSubgraph(*mainF);
-
-      auto allLoops = noelle.getLoopStructures();
-      if (allLoops->size() > 0)
-      {
-        errs() << "Example:   Fetch the LDG of the hottest loop\n";
-        noelle.sortByHotness(*allLoops);
-        auto hottestLoop = (*allLoops)[0];
-        auto ldi = noelle.getLoopContent(hottestLoop);
+    AliasAnalysisEngine *aa = nullptr;
+    for (auto e : aliasAnalysisEngines) {
+      if (e->getName() == "SVF") {
+        aa = e;
+        break;
       }
-
-      errs() << "Example: Exit\n";
-      return false;
     }
+    auto *pta = static_cast<SVF::PointerAnalysis*>(aa->getRawPointer());
 
-    void getAnalysisUsage(AnalysisUsage &AU) const override
-    {
-      AU.addRequired<NoellePass>();
+    auto computeGen = [](Instruction *i, DataFlowResult *df) {
+      if (!isa<LoadInst>(i)) {
+        return;
+      }
+      auto &gen = df->GEN(i);
+      gen.insert(i);
+    };
+
+    auto computeKILL = [](Instruction *, DataFlowResult *) { return; };
+
+    auto computeOUT = [](Instruction *i, Instruction *successor,
+                         std::set<Value *> &OUT, DataFlowResult *df) {
+      auto &inS = df->IN(successor);
+      OUT.insert(inS.begin(), inS.end());
+      return;
+    };
+
+    auto computeIN = [](Instruction *i, std::set<Value *> &IN,
+                        DataFlowResult *df) {
+      auto &genI = df->GEN(i);
+      auto &outI = df->OUT(i);
+      IN.insert(outI.begin(), outI.end());
+      IN.insert(genI.begin(), genI.end());
+    };
+
+    auto fm = noelle.getFunctionsManager();
+    auto mainF = fm->getEntryFunction();
+
+    auto customDfr = dfe.applyBackward(mainF, computeGen, computeKILL,
+                                       computeIN, computeOUT);
+
+    for (auto &F : M) {
+      for (auto &If : instructions(F)) {
+        if (auto *Lf = dyn_cast<LoadInst>(&If)) {
+          for (auto &G : M) {
+            for (auto &Ig : instructions(G)) {
+              if (auto *Lg = dyn_cast<LoadInst>(&Ig)) {
+                if (Lf == Lg) {
+                  continue;
+                }
+                llvm::Value *ptr1 = Lf->getPointerOperand();
+                llvm::Value *ptr2 = Lg->getPointerOperand();
+                SVF::SVFVar
+                auto result =
+                    pta->alias(ptr1, ptr2);
+                if (result == AliasResult::MayAlias) {
+                  errs() << "Instruction: " << Lf << " may alias " << Lg;
+                }
+                if (result == AliasResult::MustAlias) {
+                  errs() << "Instruction: " << Lf << " must alias " << Lg;
+                }
+              }
+            }
+          }
+        }
+      }
     }
-  };
-}
+    return false;
+  }
+
+  void getAnalysisUsage(AnalysisUsage &AU) const override {
+    AU.addRequired<NoellePass>();
+  }
+};
+} // namespace
 
 // Next there is code to register your pass to "opt"
 char CAT::ID = 0;
@@ -127,19 +163,15 @@ static RegisterPass<CAT> X("CAT", "Simple user of the Noelle framework");
 static CAT *_PassMaker = NULL;
 static RegisterStandardPasses _RegPass1(PassManagerBuilder::EP_OptimizerLast,
                                         [](const PassManagerBuilder &,
-                                           legacy::PassManagerBase &PM)
-                                        {
-                                          if (!_PassMaker)
-                                          {
+                                           legacy::PassManagerBase &PM) {
+                                          if (!_PassMaker) {
                                             PM.add(_PassMaker = new CAT());
                                           }
                                         }); // ** for -Ox
-static RegisterStandardPasses _RegPass2(
-    PassManagerBuilder::EP_EnabledOnOptLevel0,
-    [](const PassManagerBuilder &, legacy::PassManagerBase &PM)
-    {
-      if (!_PassMaker)
-      {
-        PM.add(_PassMaker = new CAT());
-      }
-    }); // ** for -O0
+static RegisterStandardPasses
+    _RegPass2(PassManagerBuilder::EP_EnabledOnOptLevel0,
+              [](const PassManagerBuilder &, legacy::PassManagerBase &PM) {
+                if (!_PassMaker) {
+                  PM.add(_PassMaker = new CAT());
+                }
+              }); // ** for -O0
